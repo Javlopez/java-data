3e332ae094c997401d608d99918072892728528f
Javier Lopez Lopez <sjavierlopez@gmail.com>
2018-09-18 23:15:12 -0500

    Define the infrastructure

---
+++ src/com/jlopez/managePersons/data/Connect.java
@@ -0,0 +1,63 @@
+package com.jlopez.managePersons.data;
+
+import java.sql.*;
+
+public class Connect {
+
+    private final static String JDBC_DRIVER = "com.mysql.jc.jdbc.Driver";
+    private final static String JDBC_URL = "jdbc:mysql://localhost/articles?useSSL=false";
+    private final static String JDBC_USER = "root";
+    private final static String JDBC_PASS = "rootroot";
+    private static Driver driver = null;
+
+    public static synchronized Connection getConnection() throws SQLException {
+
+        if (driver == null) {
+            try {
+                Class jdbcDriverClass = Class.forName(JDBC_DRIVER);
+                driver = (Driver) jdbcDriverClass.newInstance();
+                DriverManager.registerDriver(driver);
+            } catch(Exception e) {
+                System.out.println("The JDBC connection has a trouble, please check the logs");
+                e.printStackTrace();
+            }
+        }
+
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+    }
+
+    public static void close(ResultSet result) {
+        try {
+            if(result != null) {
+                result.close();
+            }
+
+        } catch ( SQLException sqle){
+            sqle.printStackTrace();
+        }
+    }
+
+    public static void close(PreparedStatement stmt){
+        try {
+            if(stmt != null) {
+                stmt.close();
+            }
+
+        } catch ( SQLException sqle){
+            sqle.printStackTrace();
+        }
+    }
+
+    public static void close(Connection conn){
+        try {
+            if(conn != null) {
+                conn.close();
+            }
+
+        } catch ( SQLException sqle){
+            sqle.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/com/jlopez/managePersons/domain/Person.java
@@ -0,0 +1,36 @@
+package com.jlopez.managePersons.domain;
+
+public class Person {
+    private int id;
+    private String username;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{id=" + id + ";Username=" + username + ";Email=" + email +"}";
+    }
+}
